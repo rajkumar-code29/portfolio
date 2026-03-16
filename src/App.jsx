38b40f07885460972d8ecea415abcec25ac4871e
rajkumar <raj.gk29@outlook.com>
2026-03-16 10:04:29 +0000

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-600">
+      <h1 className="text-4xl font-bold text-white underline">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
