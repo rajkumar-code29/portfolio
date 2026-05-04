246e813591af67ec5c53647779637168738c4a44
rajkumar <raj.gk29@outlook.com>
2026-05-04 08:10:21 +0100

    adding google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from 'react-ga4'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Initialize Google Analytics 
+ReactGA.initialize("G-X8TV33XYWR"); // Replace with your actual Measurement ID
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
